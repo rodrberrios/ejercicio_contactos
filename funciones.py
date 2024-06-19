06caadac7b330f498f0c30bca3680e4d94403479
rodrberrios <rodr.berrios@duocuc.cl>
2024-06-19 08:55:34 -0400

    trabajo iniciado con caleta de errores XD

---
+++ funciones.py
@@ -0,0 +1,14 @@
+def opcion_1():
+    pass
+
+def opcion_2():
+    pass
+
+def opcion_3():
+    pass
+
+def opcion_4():
+    pass
+
+def validar_int():
+    pass
--- pruebant.py
+++ pruebant.py
@@ -4,3 +4,16 @@ import time, os
 
 os.system("cls")
 
+
+while True:
+    print("""
+Menu Concactos
+
+1. Agregar 
+2. Mostrar 
+3. Guardar Archivo
+4. Salir
+
+Ingrese Opcion:""")
+    menu=int(input("> "))
+    
